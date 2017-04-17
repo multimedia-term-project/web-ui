8b0d05407e6a459bb8acfa21f2722a64778c8ca1
Kyle Fetterman <kfetterm@iupui.edu>
2017-04-16 23:20:39 -0400

    check body validation complete

--- app/routes/index.js
+++ app/routes/index.js
@@ -15,4 +15,51 @@ router.get('/login', function(req, res, next){
 	res.render('login');
 });
 
+router.post('/registerUser', function(req, res, next){
+	
+	//function that will allow us to see if the forum was empty
+	function checkBody(body){
+
+		var empty;
+		if(body){
+			empty = false;
+		}else	{
+			empty = true;
+		}
+		return empty;
+	}
+
+
+
+
+	//these variables will hold values incase we have any validation errors 
+	var displayValidation; 
+	var userNameValidation; 
+	var passwordValidation; 
+	var emailValidation; 
+	var confimPasswordValidation; 
+	var confirmEmailValidation; 
+
+	
+	//variables that hold the register User information
+	var display= req.body.display; 
+	var userName = req.body.userName; 
+	var password = req.body.password; 
+	var confirmPassword = req.body.confirmPassword; 
+	var email = req.body.email; 
+	var confirmEmail = req.body.confirmEmail;
+
+	//check to make sure that we have information in all the input boxes
+	displayValidation = checkBody(display); 
+	userNameValidation = checkBody(userName); 
+	passwordValidation = checkBody(password); 
+	emailValidation = checkBody(email);
+	confirmPasswordValidation = checkBody(confirmPasswordValidation); 
+	confirmEmailValidation = checkBody(confirmEmailValidation);
+
+	
+
+
+});
+
 module.exports = router;
